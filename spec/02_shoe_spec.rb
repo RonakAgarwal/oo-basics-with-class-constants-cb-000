6127964cab75113b48afcd575e82bb9efe9f01f8
RonakAgarwal <agarwalronak02@gmail.com>
2018-07-26 21:22:47 +0000

    automatically backed up by learn

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -1,10 +1,1 @@
-describe 'Shoe' do 
-  describe 'BRANDS' do
-    # Think about it! When does the BRANDS constant need to be
-    # made aware of the brands of the shoes that are being created?
 
-  ]
-      expect(Shoe::BRANDS.size).to eq(3)
-    end
-  end
-end
